3b2186ae786f5c414d36c908e3cdbae95921af37
kurlerprudent <osarkodie@st.ug.edu.gh>
2024-10-26 17:57:18 +0000

    New configure

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="text-yellow-500 text-6xl w-full h-screen flex justify-center items-center">
+    Hello there! This is the home page
+    </div>
+  );
+}
+
+export default App;
