cbc433fce6ec5d04fd1a6a848cdd73231deba494
“OwusuEmmanuelTakyi” <owusuemmanueltakyi@gmail.com>
2024-10-26 20:00:02 +0000

    installed the react route

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
+import Home from "./Pages/Home";
 
 
 function App() {
   return (
     <div className="text-yellow-500 text-6xl w-full h-screen flex justify-center items-center">
-    Hello there! This is the home page
+   <Home/>
     </div>
   );
 }
